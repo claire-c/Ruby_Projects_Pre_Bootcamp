8a25286936658dc525c17a9be9f8b72d89ed9f91
Claire <cconnachan@gmail.com>
2017-08-02 19:30:19 +0100

    joke.rb updated YASSSSS

--- joke.rb
+++ joke.rb
@@ -1,16 +1,7 @@
 require 'json'
 require 'net/http'
 
-site = Net::HTTP.get(URI('https://api.chucknorris.io/jokes/random'))
-
-#This all works below. But I need to figure out how to get the web page so it can then be parsed and put into function.
-
-json = File.read(site)
-
+json = Net::HTTP.get(URI('https://api.chucknorris.io/jokes/random'))
 hash = JSON.parse(json)
 
-def chucknorris(joke)
-  puts joke["value"]
-end
-
-chucknorris(hash)
+puts hash["value"]
