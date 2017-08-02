8cf3f147f950b092c0024bcdad95d2038ed47692
Claire <cconnachan@gmail.com>
2017-08-02 22:07:38 +0100

    Minor amends to joke2

--- joke2.rb
+++ joke2.rb
@@ -5,16 +5,16 @@ require 'net/http'
 json = Net::HTTP.get(URI("https://api.chucknorris.io/jokes/categories"))
 array = JSON.parse(json)
 
-puts "Jon! Choose a category and I will tell you a Chuck Norris joke..."
+puts "Jon! Choose a category and I will call an API to tell you a Chuck Norris joke... What would you like?"
 puts array
-puts "What category would you like?"
 
 category = gets.chomp.downcase
+
 if array.include?(category) == false
-  puts "That's not a valid category Jon! I need to write a loop so you can try again."
+  puts "That's not a valid category, Jon! I need to write a loop (another enemy of mine) so you can try again but until that happens you'll just need to run the app again. SORRY!"
+else
+  puts "Good choice! *clears throat*"
+  joke_json = Net::HTTP.get(URI("https://api.chucknorris.io/jokes/random?category=#{category}"))
+  joke_hash = JSON.parse(joke_json)
+  puts joke_hash["value"]
 end
-
-joke_json = Net::HTTP.get(URI("https://api.chucknorris.io/jokes/random?category=#{category}"))
-joke_hash = JSON.parse(joke_json)
-
-puts joke_hash["value"]
