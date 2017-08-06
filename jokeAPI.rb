5cd85d8f72fbb6d3fe88d74aa4a4bfc6e916bf78
Claire <cconnachan@gmail.com>
2017-08-06 19:03:52 +0100

    refactored slightly

--- jokeAPI.rb
+++ jokeAPI.rb
@@ -13,8 +13,7 @@ end
 def getRandomJoke(category_choice=nil)
   json = Net::HTTP.get(URI("https://api.chucknorris.io/jokes/categories"))
   array = JSON.parse(json)
-  array.shuffle!
-  random_category = array[0]
+  random_category = array.shuffle[0]
   joke_json = Net::HTTP.get(URI("https://api.chucknorris.io/jokes/random?category=#{random_category}"))
   joke_hash = JSON.parse(joke_json)
   puts joke_hash["value"]
