12b716424d3174b70d33e3c018b2c962c5e1e4a1
Claire <cconnachan@gmail.com>
2017-08-06 19:01:13 +0100

    added randomising function

--- jokeAPI.rb
+++ jokeAPI.rb
@@ -9,3 +9,13 @@ def getJoke(category_choice=nil)
     puts joke_hash["value"]
   end
 end
+
+def getRandomJoke(category_choice=nil)
+  json = Net::HTTP.get(URI("https://api.chucknorris.io/jokes/categories"))
+  array = JSON.parse(json)
+  array.shuffle!
+  random_category = array[0]
+  joke_json = Net::HTTP.get(URI("https://api.chucknorris.io/jokes/random?category=#{random_category}"))
+  joke_hash = JSON.parse(joke_json)
+  puts joke_hash["value"]
+end
